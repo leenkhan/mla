267ab8002d511c4b93c4ca0eb23d77006573e9a7
umark <leenkhan@outlook.com>
2018-07-26 17:25:04 +0800

    新增构造决策树方法

--- chap3/tree_test.py
+++ chap3/tree_test.py
@@ -1,7 +1,18 @@
-import chap3.trees as tress
+import chap3.trees as trees
 
-dataSet,labels =tress.createDataSet()
-print(tress.calcShannonEnt(dataSet))
+dataSet,labels =trees.createDataSet()
+print(trees.calcShannonEnt(dataSet))
 
 dataSet[0][-1] = 'maybe'
-print(tress.calcShannonEnt(dataSet))
+print(trees.calcShannonEnt(dataSet))
+
+myDat,labels = trees.createDataSet()
+res1 = trees.splitDataSet(myDat,0,1)
+print(res1)
+
+res1 = trees.splitDataSet(myDat,0,0)
+print(res1)
+
+print(trees.chooseBestFeatureToSplit(myDat))
+
+print("tree: ",trees.createTree(myDat,labels))
